8bf0f6469f1cc8d062250ac491e6481b3b8c5fbc
ALFREDOITM <alfredosanchez.as68@gmail.com>
2022-06-05 11:42:33 -0500

    .

--- app/src/main/java/com/example/finalprojectmobilepro/InsideHomeActivity.kt
+++ app/src/main/java/com/example/finalprojectmobilepro/InsideHomeActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
-import com.example.finalprojectmobilepro.databinding.ActivityMainBinding
+import com.android.volley.toolbox.Volley
 import com.example.finalprojectmobilepro.databinding.FragmentInsideHomeBinding
 import org.json.JSONArray
 
@@ -17,25 +17,26 @@ class InsideHomeActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = FragmentInsideHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        queue = Volley.newRequestQueue(this)
+        getProductList()
 
-        fun getProductList(){
-            val url = "https://fakestoreapi.com/products/?limit=9"
-            val stringRequest = StringRequest(url, Response.Listener<String>{ response ->
-                val jsonArray = JSONArray(response.toString())
-                Log.i("JSONRESPONSE", jsonArray.toString())
-                //binding..adapter = InsideHomeAdaptr(jsonArray)
-            },
-                Response.ErrorListener { error ->
-                    Log.w("JSONRESPONSE", error.message as String)
-                })
+    }
+    fun getProductList(){
+        val url = "https://fakestoreapi.com/products/?limit=9"
+        val stringRequest = StringRequest(url, Response.Listener<String>{ response ->
+            val jsonArray = JSONArray(response.toString())
+            Log.i("JSONRESPONSE", jsonArray.toString())
+            binding.rvInsideHome.adapter = InsideHomeAdapter(jsonArray)
+        },
+            Response.ErrorListener { error ->
+                Log.w("JSONRESPONSE", error.message as String)
+            })
 
-            queue.add(stringRequest)
-        }
+        queue.add(stringRequest)
+    }
 
-
-        override fun onStop() {
-            super.onStop()
-            queue.cancelAll("Stopped")
-        }
+    override fun onStop() {
+        super.onStop()
+        queue.cancelAll("Stopped")
     }
 }
--- app/src/main/java/com/example/finalprojectmobilepro/InsideHomeAdapter.kt
+++ app/src/main/java/com/example/finalprojectmobilepro/InsideHomeAdapter.kt
@@ -5,9 +5,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.finalprojectmobilepro.databinding.ItemProductRandomBinding
 import org.json.JSONArray
+import org.json.JSONObject
 
-class RandomAdapter(private val products: JSONArray): RecyclerView.Adapter<RandomAdapter.ProductHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RandomAdapter.ProductHolder {
+class InsideHomeAdapter(private val products: JSONArray): RecyclerView.Adapter<InsideHomeAdapter.ProductHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InsideHomeAdapter.ProductHolder {
         val binding = ItemProductRandomBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ProductHolder(binding);
     }
@@ -19,6 +20,13 @@ class RandomAdapter(private val products: JSONArray): RecyclerView.Adapter<Rando
     override fun getItemCount(): Int = products.length()
 
     class ProductHolder(val binding: ItemProductRandomBinding): RecyclerView.ViewHolder(binding.root){
+        fun render(product: JSONObject){
+            //binding.proName.setText(pokemon.getString("title"))
+            binding.ProductName.setText(product.getString("title"))
+            binding.ProductReviews.setText(product.getString("count"))
+            binding.ProductCost.setText((product.getString("price")))
+            binding.ProductRating.rating = product.getString("rate").toFloat()
 
+        }
     }
     }
